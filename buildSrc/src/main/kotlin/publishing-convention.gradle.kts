4266daf1b89b1eb93361ca9733601d9a2293fd0a
Scott <schott12521@gmail.com>
2024-11-24 18:26:10 -0800

    fix: github credentials being passed into publishing-convention.gradle.kts

--- buildSrc/src/main/kotlin/publishing-convention.gradle.kts
+++ buildSrc/src/main/kotlin/publishing-convention.gradle.kts
@@ -11,8 +11,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/schott12521/ExampleLibrary")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
