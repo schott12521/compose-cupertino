4f7ff6f10eaa17950e323f98d233b79ba19e1f89
Scott Lanoue <schott12521@gmail.com>
2025-01-14 11:48:35 -0800

    fix: Fix license and reduce min android sdk (#33)
    * fix: fixing licences and copyright
    
    * fix: update android min sdk
    
    * fix: Update README
    
    * fix: more updates to readme

--- cupertino-native/src/iosMain/kotlin/com/slapps/cupertino/CupertinoColorPickerNative.ios.kt
+++ cupertino-native/src/iosMain/kotlin/com/slapps/cupertino/CupertinoColorPickerNative.ios.kt
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2023-2024 Compose Cupertino project and open source contributors.
+ * Copyright (c) 2023-2024. Compose Cupertino project and open source contributors.
+ * Copyright (c) 2025. Scott Lanoue.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
