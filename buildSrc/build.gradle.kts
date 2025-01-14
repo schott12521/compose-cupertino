01eeebc7b92446c5b6ee65a7f115ed30f7320f06
Scott Lanoue <schott12521@gmail.com>
2025-01-13 17:19:12 -0800

    fix: still battling against CI (#29)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,6 @@ dependencies {
     implementation(gradleApi())
     implementation(libs.gradle)
     implementation(libs.kotlin.gradle.plugin)
-    implementation(libs.vanniktech.mavenPublish)
+//    implementation(libs.vanniktech.mavenPublish)
+    implementation(libs.vanniktech.maven.publish.gradle.plugin)
 }
