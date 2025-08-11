e7e6efb0a5a1577049c36406bc8a45f96a1b064b
Dmitry <kalistratov.d.m@gmail.com>
2025-08-12 01:15:38 +0400

    Update Kotlin to 2.2.0 and Gradle to 8.12.0.

--- example/composeApp/build.gradle.kts
+++ example/composeApp/build.gradle.kts
@@ -58,7 +58,7 @@ kotlin {
 
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        moduleName = "composeApp"
+        outputModuleName = "composeApp"
         browser {
             commonWebpackConfig {
                 outputFileName = "composeApp.js"
